8df1cf1d09acbf4f93c211fc09bbbf01276c8b2a
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-13 18:04:13 +0200

    [ADD] aggiunto conto alla rovescia

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const contoAllaRovescia = 10;
+
+// creo la funzione Timeout
+
+setTimeout(function() {
+    // inserisco allert
+    alert('Buon anno!')
+}, contoAllaRovescia * 1000
+
+);
