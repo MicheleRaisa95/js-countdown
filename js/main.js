d407369ed7e8caaa3e80c2e6673921150831ee34
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-14 09:59:22 +0200

    aggiunte correzione viste durante la lezione

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 'use strict'
 
-const contoAllaRovescia = 10;
+let contoAllaRovescia = 10;
 
 // creo la funzione Timeout
 
-setTimeout(function() {
+/*setTimeout(function() {
     // inserisco allert
     alert('Buon anno!')
 }, contoAllaRovescia * 1000
 
-);
+);*/
+
+const clock = setInterval(function () {
+    if (contoAllaRovescia === 0) {
+        alert( 'Buon anno!');
+        clearInterval(clock);
+    } else {
+        console.log(contoAllaRovescia);
+        contoAllaRovescia--;
+    }
+}, 1000);
